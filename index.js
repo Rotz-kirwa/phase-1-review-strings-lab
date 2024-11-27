38f979a5dd71221be7ecf11637847153770c907d
rotzkirwa <eliud.rotich@student.moringaschool.com>
2024-11-27 12:42:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable and assign it a string value
+const currentUser = "Grace Hopper";
+
+// Define the welcomeMessage variable and use string interpolation to incorporate currentUser
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable by converting welcomeMessage to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting variable using the first letter of currentUser
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
